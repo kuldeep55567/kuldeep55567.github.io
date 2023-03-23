5dd57f4054dca613b72de7f3d997fc5e7a37a322
kuldeep55567 <kanj55567@gmail.com>
2023-03-23 13:03:37 +0530

    Portfolio revised

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const swiper = new Swiper('.swiper',{
-    centeredSlides: true,
+const swiper = new Swiper('.swiper', {
+    loop: true,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-})
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
 // ............navbar..........
 document.querySelector('.nav-button .Open').addEventListener('click',()=>{
     document.querySelector('nav .nav-container ul').style.left = '0';
     document.querySelector('.nav-button .Close').style.display='block'
     document.querySelector('.nav-button .Open').style.display='none'
 });
-
 document.querySelector('.nav-button .Close').addEventListener('click',()=>{
     document.querySelector('.nav-button .Close').style.display='none'
     document.querySelector('nav .nav-container ul').style.left = '-16rem';
